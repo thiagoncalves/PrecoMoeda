98a63e40c91059525be1d2b73e977e69edb331f2
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-22 23:31:26 -0300

    Novo Projeto

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecoMoeda
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Digite um valor em real para conversão em moedas estrangeiras");
+            Console.Write("R$: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            double valorDolar = ConversaoDollar(valor);
+            double valorEuro = ConversaoEuro(valor);
+            double valorYen = ConversaoYen(valor);
+
+            Console.WriteLine("O valor convertido para dolar é $" + valorDolar.ToString(".00"));
+            Console.WriteLine("O valor convertido para euro é €" + valorEuro.ToString(".00"));
+            Console.WriteLine("O valor convertido para yen é ¥" + valorYen.ToString(".00"));
+
+            Console.ReadLine();
+
+        }
+
+        public static double ConversaoDollar (int valor)
+        {
+            double resultado = valor / 5.53;
+            return resultado;
+        }
+
+        public static double ConversaoEuro (int valor)
+        {
+            double resultado = valor / 6.03;
+            return resultado;
+        }
+
+        public static double ConversaoYen (int valor)
+        {
+            double resultado = valor / 0.051;
+            return resultado;
+        }
+    }
+}
